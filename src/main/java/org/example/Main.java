4945217c3e2d3ff512e5a6736ecf9bb724f7d7dd
WeiberKev <158856366+WeiberKev@users.noreply.github.com>
2025-06-03 09:53:08 -0500

    refactor actualizacíon de la app

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,46 @@
 
 package org.example;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Reservas APP");
+        System.out.println("=================\n");
+
+        //Entradas del algoritmo
+        Scanner entradaTeclado=new Scanner(System.in);
+        LocalDate fechaReserva;
+        LocalDate fechaMantenimiento;
+
+        String nombreUsuario;
+        String idUsuario;
+        String correoUsuario;
+        String tipoUsuario;
+        String numeroApartamento;
+        String tipoApartamento;
+        String contraseñaUsuario;
+        String telefonoUsuario;
+
+        String horarioEspacio;
+        String nombreEspacio;
+        String reglamentoEspacio;
+        String utensiliosEspacio;
+
+        String codigoReserva;
+        String horarioReserva;
+
+        byte numeroBloque;
+        byte cantidadAsistentes;
+        Integer valorReserva;
+
+        boolean perfilUsuario=false;
+        boolean confirmacionReserva=false;
+        boolean estaOcupado=false;
+        boolean estaEnMantenimiento=false;
+
+
     }
 }
